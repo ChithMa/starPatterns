7c0cf8584958483ebe5957d7a317d654a816fe00
Chithma Pathirana <manangapathirana@gmail.com>
2025-06-16 08:37:47 +0530

    Mario Pyramids
    Mario 2 Pyramids

---
+++ marioPyramids.c
@@ -0,0 +1,43 @@
+//    # #
+//   ## ##
+//  ### ###
+// #### ####
+
+
+#include "cs50.h"
+#include <stdio.h>
+
+int main (void)
+{
+    int height = get_int("Height: ");
+    if (height <= 0 || height > 50)
+    {
+        printf("Invalied Height! height should be between 0 and 50");
+        return 1;
+    }
+
+    for (int i = 0; height > i; i++)
+    {
+        for (int j = height; j > i; j--)
+        {
+            printf(".");
+            
+            
+        }
+        for (int k = 0; k < i+1; k++)
+        {
+            printf("#");
+            
+        }
+        printf(" ");
+        for (int k = 0; k < i+1; k++)
+        {
+            printf("#");
+            
+        }
+        printf("c");
+        printf("\n");
+       
+
+    }
+}
